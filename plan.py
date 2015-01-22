2807c2fb286a9386d86f67b918d160dcc6a45f3e
bing <bingxuhu@hotmail.com>
2015-01-21 23:58:38 -0500

    Plan
    The Strategy to solve

---
+++ plan.py
@@ -0,0 +1,8 @@
+# Let us assume the raw data is in the form of an array that is 16 by 30
+# Let us assume there are 99 mines in total 
+
+# There are two systems where we solve one square
+# 1. Guessing
+#    * For the lolz, lets implement a monte carlo search tree for this portion
+# 2. Calculating
+
