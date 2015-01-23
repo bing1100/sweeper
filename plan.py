49841d47004955f1f72e6e588e827b153ddf527b
bing1100 <bingxuhu@hotmail.com>
2015-01-23 16:00:37 -0500

    Main C

--- plan.py
+++ plan.py
@@ -6,3 +6,13 @@
 #    * For the lolz, lets implement a monte carlo search tree for this portion
 # 2. Calculating
 
+# Calculating module
+
+# Each square has 8 other squares that can effect the squares outcome 
+
+# Each individual square has power in determining the location of the mine
+# and the likely hood of there being a mine around it 
+
+# 16 by 30 array
+
+# number on the map is represented as 
