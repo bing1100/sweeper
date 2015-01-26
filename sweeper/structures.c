52d6de45d0c2067d546e5887431757bf1007afa0
bing <bingxuhu@hotmail.com>
2015-01-25 23:10:23 -0500

    structure
    constructor part 1 complete

--- sweeper/structures.c
+++ sweeper/structures.c
@@ -13,7 +13,7 @@
 #  then the u of llnode y is x
 
 # int hg (horizontal grid) and vg (vertical grid) represent where the llnode is on the 30 by 16 grid
-# and these values are utilised to generate the 30 by 16 grid.
+# and these values are utilised to generate the 30 by 16 grid. (top and left is zero and increases)
 
 struct llnode {
     int hg;
@@ -37,3 +37,53 @@ struct llnode {
 # and one constructor that constructs to the right
 
 
+# this is the constructor function that constructs down
+
+struct llnode *cons_down(int ivg,int ihg,struct llnode *initial,int b, struct llnode *r){
+    struct llnode *new = malloc(sizeof(struct llnode));
+    new->hg = r->hg;
+    new->vg = r->vg + 1;
+    new->item = -1;
+    new->u = r;
+    r->b = new;
+    new->ur = NULL;
+    new->r = NULL;
+    new->br = NULL;
+    new->b = NULL;
+    if(r->left != NULL){
+        new->l = r->bl;
+        new->bl = r->bl->b;
+        new->ul = r->u;
+
+        r->bl->r = new;
+        r->bl->br = new->b;
+        r->bl->ur = r;
+    }
+    else {
+        new->l = new->bl = new->ul = NULL;
+    }
+    return new;
+};
+
+#this is the constructor function that constructs right
+
+struct llnode *cons_right(int ivg,int ihg,struct llnode *initial, struct llnode *r){
+    struct llnode *new = malloc(sizeof(struct llnode));
+    new->hg = r->hg;
+    new->vg = r->vg + 1;
+    new->item = -1;
+    new->l = r;
+    r->r = new;
+    new->bl = r->b;
+    new->b = NULL;
+    new->r  = NULL;
+    new->br = NULL;
+    new->ul = NULL;
+    new->u = NULL;
+    new->ur =NULL;
+    return new;
+};
+
+void cons_grid(int hs, int vs){
+
+}
