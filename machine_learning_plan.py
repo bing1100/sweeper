eb576a30f9e79840196bd0cde8674f8a60553cc8
bing1100 <bingxuhu@hotmail.com>
2015-01-27 14:14:08 -0500

    solvemodule Plan
    v1

---
+++ machine_learning_plan.py
@@ -0,0 +1,13 @@
+# Inteligence module
+
+# Going to use a decision tree to record patterns and so that
+#  the program will run faster as it collects more information
+
+# The decision tree will work in this way:
+#  We will have a fixed algorithm to look for patterns.
+#  The decision tree will be updated every time a pattern has been
+#  solved.
+# Each node represents the value of a block chosen through the algorithm
+
+# Higher levels can be implemented (higher levels means we investigate outside of
+#  the set block)
