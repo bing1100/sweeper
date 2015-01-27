a436fa9d236c6f4a747423fb2558f2223084a0e3
bing <bingxuhu@hotmail.com>
2015-01-27 17:59:33 -0500

    solvemodule v1

--- sweeper/solvemodule.c
+++ sweeper/solvemodule.c
@@ -1,1 +1,62 @@
+int check_known(struct llnode *block){
+    int count = 0;
+    if(block->ul > -1){
+            count++;
+    }
+    if(block->u > -1){
+            count++;
+    }
+    if(block->ur > -1){
+            count++;
+    }
+    if(block->r > -1){
+            count++;
+    }
+    if(block->br > -1){
+            count++;
+    }
+    if(block->b > -1){
+            count++;
+    }
+    if(block->bl > -1){
+            count++;
+    }
+    if(block->l > -1){
+            count++;
+    }
+
+    return count;
+}
+
+
+int check_bomb(struct llnode *block){
+    int count = 0;
+    if(block->ul == -2){
+            count++;
+    }
+    if(block->u == -2){
+            count++;
+    }
+    if(block->ur == -2){
+            count++;
+    }
+    if(block->r == -2){
+            count++;
+    }
+    if(block->br == -2){
+            count++;
+    }
+    if(block->b == -2){
+            count++;
+    }
+    if(block->bl == -2){
+            count++;
+    }
+    if(block->l == -2){
+            count++;
+    }
+
+    return count;
+}
+
 
