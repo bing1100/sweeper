c31965c9bec9a6d2e13ab410ddd918a63f09cde3
bhux <bhux@localhost.(none)>
2015-08-25 17:21:16 -0400

    C++ file + UML Diagram

---
+++ c++/minesweeper/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: bhux
+ *
+ * Created on August 25, 2015, 4:21 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
